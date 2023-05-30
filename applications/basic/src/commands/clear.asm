9b6d8d5df6439b6fd47c8232463a18dd8cc049b4
Paul Robson <paul@robsons.org.uk>
2023-05-30 11:56:38 +0100

    Update

--- applications/basic/src/commands/clear.asm
+++ applications/basic/src/commands/clear.asm
@@ -45,6 +45,47 @@ Command_CLEAR:	;; [clear]
 		jsr 	StringSystemInitialise 		
 		rts
 
+; ************************************************************************************************
+;
+;								Allocate XA bytes all zeroed
+;
+; ************************************************************************************************	
+
+AllocateMemory:
+		phy 								; save Y
+		ldy 	freeMemory 					; save addr.low
+		phy
+		ldy 	freeMemory+1 				; save addr.high
+		ply
+		tay 								; count is now in XY
+_AllocateLoop:
+		cpx 	#0 							; allocate count is zero ?
+		bne 	_AllocateOne
+		cpy 	#0
+		beq 	_AllocateExit 
+_AllocateOne:		
+		;
+		lda 	#0 							; zero byte
+		sta 	(freeMemory)
+		;
+		inc 	freeMemory 					; bump pointer
+		bne 	_AllocateSkipCarry
+		inc 	freeMemory+1
+_AllocateSkipCarry:
+		;
+		cpy 	#0 							; decrement XY
+		bne 	_AllocateSkipBorrow
+		dex
+_AllocateSkipBorrow:
+		dey
+		bra 	_AllocateLoop				
+		;
+_AllocateExit:
+		plx 								; restore address
+		pla		
+		ply 								; restore Y
+		rts
+
 		.send code
 
 		.section zeropage
--- applications/basic/src/variables/create.asm
+++ applications/basic/src/variables/create.asm
@@ -19,6 +19,58 @@
 		.section code
 
 VARCreate:
+		;
+		;		Allocate memory
+		;
+		lda 	#9 							; create 9 bytes of space
+		ldx 	#0
+		jsr 	AllocateMemory
+		sta 	zTemp1 						; save new address in zTemp1
+		stx 	zTemp1+1
+		;
+		;		Fill in hash and reference to variable name
+		;
+		ldy 	#2 							; put hash into +2
+		lda 	VARHash
+		sta 	(zTemp1),y
+		;
+		iny 								; put address of name into +3,+4
+		lda 	VARNameAddress
+		sta 	(zTemp1),y
+		iny
+		lda 	VARNameAddress+1
+		sta 	(zTemp1),y
+		;
+		;		Link into hash table - old head becomes link on new record
+		;		(inserted into front)
+		;
+		lda 	VARHashEntry 				; hash table ptr -> zTemp0
+		sta 	zTemp0
+		lda 	VARHashEntry+1
+		sta 	zTemp0+1
+		;
+		ldy 	#1 							; put current head into link.
+		lda 	(zTemp0)
+		sta 	(zTemp1)
+		lda 	(zTemp0),y
+		sta 	(zTemp1),y
+		;
+		;		New Record is the new head
+		;
+		lda 	zTemp1 						; address of the new record into head
+		sta 	(zTemp1)
+		lda 	zTemp1+1
+		sta 	(zTemp1),y
+		;
+		;		Make XA point to the data part of the variable (e.g. offset 5)
+		;
+		lda 	zTemp1 						; new record to XA
+		ldx 	zTemp1+1
+		clc 								; add 5 to point to the data.
+		adc 	#5
+		bcc 	_VCNoCarry
+		inx
+_VCNoCarry:		
 		rts
 
 		.send code
