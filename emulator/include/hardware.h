d88904a62d3eaeb1f83ff16b817427891ce157c6
Paul Robson <paul@robsons.org.uk>
2023-06-27 15:43:26 +0100

    Update

--- emulator/include/hardware.h
+++ emulator/include/hardware.h
@@ -17,4 +17,13 @@ void HWSync(void);
 void HWQueueKeyboardEvent(int scanCode);
 
 int HWReadScancodeQueue(void);
+
+int HWFlashCommand(int command,int data);
+
+#define HWF_ERASE  			(0x00)		 			// Erase sector.
+#define HWF_OPENREAD 		(0x01) 					// Open sector to read
+#define HWF_OPENWRITE  		(0x02) 					// Open sector to write
+#define HWF_READ  			(0x03)					// Read one byte
+#define	HWF_WRITE  			(0x04) 					// Write one byte
+
 #endif
