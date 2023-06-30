5e25c0dbb4ac448a2ea9e24d753a9cdb4b1f4b1a
Paul Robson <paul@robsons.org.uk>
2023-06-30 06:16:18 +0100

    Update

--- applications/basic/documents/assem2.py
+++ applications/basic/documents/assem2.py
@@ -2,6 +2,17 @@
 def rol(n):
 	return (n << 1) + (0 if (n & 0x80) == 0 else 1)
 
+def hash(op):
+	values = [ord(c)-ord('A') for c in op.upper()]
+	calc = values[0]
+	calc = (calc * multiplier + additive ) & 0xFF
+	calc = (calc + values[1]) & 0xFF
+	calc = rol(calc)
+	calc = calc ^ xor
+	calc = (calc * multiplier + additive ) & 0xFF
+	calc = (calc + values[2]) & 0xFF
+	return calc
+
 opcodes = """
 	ADC AND ASL BCC BCS BEQ BIT BMI BNE BPL BRA BRK BVC BVS CLC CLD CLI CLV CMP CPX CPY DEC DEX DEY EOR INC INX INY JMP	
 	JSR LDA LDX LDY LSR NOP ORA PHA PHP PHX PHY PLA PLP PLX PLY ROL ROR RTI RTS SBC SEC SED SEI STA STX STY STZ TAX TAY
@@ -16,17 +27,7 @@ additive = 68
 xor = 165
 
 for op in opcodes:
-
-	values = [ord(c)-ord('A') for c in op]
-
-	calc = values[0]
-	calc = (calc * multiplier + additive ) & 0xFF
-	calc = (calc + values[1]) & 0xFF
-	calc = rol(calc)
-	calc = calc ^ xor
-	calc = (calc * multiplier + additive ) & 0xFF
-	calc = (calc + values[2]) & 0xFF
-
+	calc = hash(op)
 	assert mapping[calc] is None,op
 	mapping[calc] = op
 
